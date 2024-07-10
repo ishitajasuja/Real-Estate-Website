07f4bc54b9605343545ca982040f80e092ed12c6
Ishita Jasuja <jasujaishita1309@gmail.com>
2024-07-10 14:24:53 +0530

    header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import Home from "./pages/Home"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import About from "./pages/About"
+import Profile from "./pages/Profile"
+import Header from "./components/header"
+
+
 export default function App() {
   return (
-    <h1 className="text-red-600">
-      Hello world!
-    </h1>
+   <BrowserRouter>
+     <Header/>
+   <Routes>
+  
+    <Route path="/" element={<Home></Home>}></Route>
+    <Route path="/sign-in" element={<SignIn></SignIn>}></Route>
+    <Route path="/sign-out" element={<SignUp></SignUp>}></Route>
+    <Route path="/about" element={<About></About>}></Route>
+     <Route path="/profile" element={<Profile></Profile>}></Route>
+   </Routes>
+   </BrowserRouter>
   )
 }
