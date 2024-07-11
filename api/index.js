16a425bf2a7bbe65d9fe1e215eb0a93c9be033cd
Ishita Jasuja <jasujaishita1309@gmail.com>
2024-07-11 15:11:14 +0530

    coonection to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
-const express=require("express")
-const app = express();
-app.listen(3000,()=>{
-    console.log("listening on port 3000...")
-})
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const { error } = require("console");
+dotenv.config();
+
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("db connected");
+  })
+  .catch((err) => console.log("error in db connection"));
+
+  const app = express();
+app.listen(3000, () => {
+  console.log("listening on port 3000...");
+});
