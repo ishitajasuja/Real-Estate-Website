0c249a73cddf726d2499ec8c2c8ed23110552de9
Ishita Jasuja <jasujaishita1309@gmail.com>
2024-07-11 15:39:57 +0530

    user model created

---
+++ models/user.model.js
@@ -0,0 +1,23 @@
+const mongoose=require("mongoose")
+const userSchema = mongoose.Schema({
+  username: {
+    required: true,
+    unique: true,
+    type: String,
+  },
+
+  email: {
+    required: true,
+    unique: true,
+    type: String,
+  },
+
+  password: {
+    required: true,
+    type: String,
+  },
+} , {timestamps: true});
+
+const User=mongoose.model('User',userSchema);
+
+export default User;
