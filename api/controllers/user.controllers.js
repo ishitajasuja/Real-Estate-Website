1035c918f529fe3a09a276f87726e112557bb0d6
Ishita Jasuja <jasujaishita1309@gmail.com>
2024-07-11 17:03:15 +0530

    test api working

---
+++ api/controllers/user.controllers.js
@@ -0,0 +1,7 @@
+ const test =
+ 
+  (req, res) => {
+    res.send("hello haha");
+  };
+
+  module.exports=test;
--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const { error } = require("console");
 dotenv.config();
-
+const userRouter=require("./routes/user.route");
 
 mongoose
   .connect(process.env.MONGO)
@@ -13,6 +13,6 @@ mongoose
   .catch((err) => console.log("error in db connection"));
 
   const app = express();
-app.listen(3000, () => {
-  console.log("listening on port 3000...");
-});
+app.use("/api/user",userRouter);
+
+app.listen(3000 , ()=>{console.log("running on port 3000")});
